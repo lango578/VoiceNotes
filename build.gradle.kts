4c34be13a501ce89673827743a9298831a78e6d5
lango578 <huix2048@gmail.com>
2026-08-11 10:01:42 +0800

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// 项目级构建脚本：统一声明插件版本（由 settings.gradle.kts 引入）
+plugins {
+    id("com.android.application") version "8.10.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
